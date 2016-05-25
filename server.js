25330c288470290d861fc3c9994d9115a37d7cbc
Solveig Løvhaug <solveig@bedredata.no>
2016-05-25 18:17:03 +0200

    amp-list

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-//res.sendFile('/BedreSafari.html');
-  res.send('Hello World!')
+res.sendFile('/BedreSafari.html');
+//  res.send('Hello World!')
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
